8ed8178d64d4c6983d257f4ef24bdf15dc411847
hesh <1035922783@qq.com>
2018-07-23 11:11:33 +0800

    路径动画的处理

--- web/js/index.js
+++ web/js/index.js
@@ -1,7 +1,12 @@
 /**
  * Created by He on 2018/7/22.
  */
-var swipe = swipe($('#content'));
+var container = $('#content');
+var swipe = swipe(container);//初始化滑动对象
+
+//页面可视区域
+var visualWidth = container.width();
+var visualHeight = container.height();
 
 //获取数据
 var getValue = function (className) {
@@ -35,34 +40,82 @@ $boy.css({
 // });
 
 /*结合精灵动画和transition可以实现走路动态
-*通过引入jquery.transit的封装实现transition渐变的方式让人物走动*/
+ *通过引入jquery.transit的封装实现transition渐变的方式让人物走动*/
 // $('button').click(function () {
 //    $boy.transition({
-//        'left':$('#content').width() + 'px'
+//        'left':container.width() + 'px'
 //    },10000,'linear',function () {});
 // });
 
 /*添加暂停功能*/
+/*
+ $('button:first').click(function () {
+ $boy.addClass('slowWalk').transition({
+ 'left':container.width() + 'px'
+ },10000);
+ });
+ //暂停
+ $('button:nth-child(2)').click(function () {
+ // 强制做了一个改变目标left的处理
+ // 动画是要运行10秒,所以此时动画还是没有结束的
+ var currentLeft = $boy.css('left');
+ $boy.css('left',currentLeft);
+ $boy.removeClass('recoverWalk');
+ $boy.addClass('pauseWalk');
+ });
+
+ //恢复
+ $('button:last').click(function () {
+ $boy.removeClass('pauseWalk');
+ $boy.addClass('slowWalk').transition({
+ 'left':container.width() + 'px'
+ },10000);
+ $boy.addClass('recoverWalk');
+ });*/
+
+///////////////////////动画处理/////////////////////////////
+
 $('button:first').click(function () {
-    $boy.addClass('slowWalk').transition({
-        'left':$('#content').width() + 'px'
-    },10000);
-});
-//暂停
-$('button:nth-child(2)').click(function () {
-    // 强制做了一个改变目标left的处理
-    // 动画是要运行10秒,所以此时动画还是没有结束的
-    var currentLeft = $boy.css('left');
-    $boy.css('left',currentLeft);
-    $boy.removeClass('recoverWalk');
-    $boy.addClass('pauseWalk');
+    // 10秒钟 ，走到0.5 也就是页面中间位置
+    var distX = calculateDist('x', 0.5);
+    var distY = calculateDist('y', 0.5);
+    walkRun(10000, distX, distY);
 });
 
-//恢复
-$('button:last').click(function () {
+//恢复走路
+function restoreWalk() {
     $boy.removeClass('pauseWalk');
-    $boy.addClass('slowWalk').transition({
-        'left':$('#content').width() + 'px'
-    },10000);
-    $boy.addClass('recoverWalk');
-});
+}
+
+//css3的脚步动作变化
+function slowWalk() {
+    $boy.addClass('slowWalk');
+}
+
+//计算移动距离
+function calculateDist(direction, proportion) {
+    return (direction == 'x' ? visualWidth : visualHeight) * proportion;
+}
+
+//用transition做走路运动
+function startRun(options, runTime) {
+    var dfdPlay = $.Deferred();
+    //恢复走路
+    restoreWalk();
+    //运动的属性
+    $boy.transition(options, runTime, 'linear', function () {});
+    return dfdPlay;
+}
+
+//开始走路
+function walkRun(time, distX, distY) {
+    time = time || 3000;
+    //脚动作
+    slowWalk();
+    //走路运动
+    var dfd = startRun({
+        'left': distX + 'px',
+        'top': distY ? distY : undefined
+    }, time);
+    return dfd;
+}
